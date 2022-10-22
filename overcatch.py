e6e2ccbebf16dfbb4b4eeee6ac486eb6e0f595bd
곽태영 <amberaplin14@gmail.com>
2022-10-22 16:52:25 +0900

    ishack discrimination algorithm
    [핵 판별 알고리즘 수정]
    *yolov5 내 새로 추가된 파일 목록
    - downsize.py -> 학습 중간에 중단된 파일 용량 줄이기
    - dist.py -> 킬사인 프레임 추출, 킬사인 기준 이전 30프레임 분석해 킬사인에 대한 타겟 파악, 30프레임 데이터 저장(보간, 이상치 제거 등)
    
    * yolov5 수정된 파일
    - detect.py -> dist 파일 저장하도록
    - utils 폴더 내 plots.py -> 조준점과 오브젝트 헤드 추정 부분(중점에서 조금 위) 사이 거리 시각화
    
    * 핵 판별 결과 반환 코드
    - overcatch.py -> dist.py 및 detect.py 참조하여 lstm모델 통과, 결과 반환

---
+++ overcatch.py
@@ -0,0 +1,75 @@
+from yolov5.detect import Inference
+from yolov5.dist import Dist
+from pathlib import Path
+import os
+import sys
+import numpy as np
+
+from pandas import Series
+import tensorflow as tf
+import keras
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
+class OverCatch:
+    weights = ROOT / 'yolov5' / 'datasets' / 'oc1_oc2_best.pt'
+    model_dist = ROOT / 'yolov5' / 'datasets' / 'dist_model_vanilla.h5'
+    imgsz = (1080, 1920)
+    conf = 0.1
+    source = ''
+    project = ROOT / 'result'
+    name = ''
+    data = ROOT / 'datasets' / 'data.yaml'
+    nosave = False
+    save_dist = True
+    save_conf = True
+    exist_ok = True
+
+    def __init__(self, filename):
+        OverCatch.name = str(Path(filename).stem)
+        OverCatch.source = ROOT / 'original' / '{}'.format(filename)
+
+    @staticmethod
+    def predict():
+        if not os.path.exists(OverCatch.project):
+            os.makedirs(OverCatch.project)
+        Inference.run(weights=OverCatch.weights, imgsz=OverCatch.imgsz, conf_thres=OverCatch.conf,
+                                 source=OverCatch.source, project=OverCatch.project, name=OverCatch.name,
+                                 data=OverCatch.data, nosave=OverCatch.nosave, save_dist=OverCatch.save_dist,
+                                 save_conf=OverCatch.save_conf)
+        dist_filepath = str(ROOT / 'result' / OverCatch.name / 'dist' / '{}_dist.txt'.format(OverCatch.name))
+        ks_frame_target, dist, dist_diff, dist_vanilla = Dist.get_dict(dist_filepath)
+        
+        #dist
+        sequences = list()
+        for k, v in dist_vanilla.items():
+            sequences.append(Series(v).fillna(0).tolist())
+        padded_inputs = tf.keras.preprocessing.sequence.pad_sequences(sequences,
+                                                                      padding='post',
+                                                                      truncating='post',
+                                                                      dtype='float',
+                                                                      maxlen=30,
+                                                                      value=0)
+        model = keras.models.load_model(OverCatch.model_dist)
+        pred = model.predict(padded_inputs)
+        test_preds = (pred>0.5).astype("int32")
+        # tp = model.predict(padded_inputs).astype("float64")
+        # print(tp)
+        # hack = 0
+        # for i in range(len(test_preds)):
+        #     if test_preds[i][0] == 1:
+        #         hack += test_preds[i][0]
+
+        # print('Hack : {}, General : {}'.format(hack / len(test_preds), 1 - (hack / len(test_preds))))
+        # if hack / len(test_preds) > (1 - (hack / len(test_preds))):
+        #     print('핵 사용 의심됨!')
+        # else:
+        #     print('핵을 사용하지 않음으로 판단됨.')
+
+        percent = np.mean(pred.flatten(order='C').tolist())
+        return percent
